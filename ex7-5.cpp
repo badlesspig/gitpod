3d6c8118e58323260928138474c41e746c4ebe3c
badlesspig <69028968+badlesspig@users.noreply.github.com>
2022-06-28 20:52:44 +0800

    Update ex7-5.cpp

--- ex7-5.cpp
+++ ex7-5.cpp
@@ -2,18 +2,18 @@
 
 int main()
 {
-    float ttm(float a, float b);
+    float mtt(float a, float b);
     float n1,n2,x;
     printf("\nhours: ");
     scanf("%f",&n1);
     printf("\nminutes: ");
     scanf("%f",&n2);
-    x = ttm(n1, n2);
+    x = mtt(n1, n2);
     printf("\nYour time (hours) = %f\n",x);
     return 0;
 }
 
-int ttm(float a, float b)
+int mtt(float a, float b)
 {
     float result = a+b/60;
     return result;                     
